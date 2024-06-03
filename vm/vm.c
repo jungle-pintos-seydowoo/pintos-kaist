1daa9248378085787e9430c14f4b97bd21c9b7f0
syk25 <syk2245@gmail.com>
2024-06-03 15:38:40 +0000

    frame 관리함수 구현

--- vm/vm.c
+++ vm/vm.c
@@ -115,7 +115,17 @@ vm_evict_frame (void) {
 static struct frame *
 vm_get_frame (void) {
 	struct frame *frame = NULL;
-	/* TODO: Fill this function. */
+	
+	/* 사용자 풀에서 메모리를 할당하고 커널가상주소를 반환(불가능할 경우, NULL 반환) */
+	void *kva = palloc_get_page(PAL_USER);
+
+	// 할당 가능한 페이지가 없는 경우 패닉
+	if(kva == NULL) PANIC("TODO");
+
+	// 프레임 초기화
+	frame = malloc(sizeof(struct frame));
+	frame->kva = kva;
+
 
 	ASSERT (frame != NULL);
 	ASSERT (frame->page == NULL);
@@ -157,20 +167,26 @@ bool
 vm_claim_page (void *va UNUSED) {
 	struct page *page = NULL;
 	/* TODO: Fill this function */
+	page = spt_find_page(&thread_current()->spt,va);
 
+	if(page == NULL) return false;
 	return vm_do_claim_page (page);
 }
 
 /* Claim the PAGE and set up the mmu. */
 static bool
 vm_do_claim_page (struct page *page) {
+	/* 프레임 가져옴 */
 	struct frame *frame = vm_get_frame ();
 
-	/* Set links */
+	/* 페이지와 프레임 매핑 */
 	frame->page = page;
 	page->frame = frame;
 
-	/* TODO: Insert page table entry to map page's VA to frame's PA. */
+	/* TODO: pml4 매핑? */
+	struct thread *current = thread_current();
+	pml4_set_page(current->pml4, page->va, frame->kva, page->writable);
+
 
 	return swap_in (page, frame->kva);
 }
